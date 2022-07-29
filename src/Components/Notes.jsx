98963270b00b49895f753e50c227087060df33c2
Subhash <108679254+IronCode0@users.noreply.github.com>
2022-07-29 20:13:37 +0530

    Initial commit

---
+++ src/Components/Notes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Time in Finland</h1>
+      <p>
+        Finland uses Eastern European Time (EET) during the winter as standard
+        time and Eastern European Summer Time (EEST) during the summer as
+        daylight saving time. EET is two hours ahead of coordinated universal
+        time and EEST is three hours ahead of coordinated universal time.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
